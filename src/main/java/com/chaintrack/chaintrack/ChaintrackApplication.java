6f992021f72010be35a62b7cbe6ca2c6bc2132cc
Yared Kiros <19re1kiros2d94@gmail.com>
2026-05-09 17:34:24 +0300

    chore(init): initialize Spring Boot 3. project via Spring Initializr with Maven

---
+++ src/main/java/com/chaintrack/chaintrack/ChaintrackApplication.java
@@ -0,0 +1,13 @@
+package com.chaintrack.chaintrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaintrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaintrackApplication.class, args);
+    }
+
+}
