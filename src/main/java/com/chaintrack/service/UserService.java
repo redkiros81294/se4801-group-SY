2e0c8a09f6b07053e526f0b9b9e91802b285a66a
Simon Mesfin <simon2130@example.com>
2026-05-28 09:23:52 +0300

    test(conf): add test JWT secret for JwtUtils validation in tests

--- src/main/java/com/chaintrack/service/UserService.java
+++ src/main/java/com/chaintrack/service/UserService.java
@@ -5,8 +5,6 @@ import com.chaintrack.dto.response.UserResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-
-
 public interface UserService {
 
     UserResponse register(RegisterRequest request);
